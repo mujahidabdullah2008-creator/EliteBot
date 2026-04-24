d1a76fa8f5c52d1097f30cc19bd95e580c63fe3e
mujahidabdullah2008-creator <mujahidabdullah2008@gmail.com>
2026-04-24 15:45:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-# main.py
 import time
 
 print("🚀 Bot started...")
 
 while True:
-    try:
-        print("Scanning market...")
-        # your logic here
-
-        time.sleep(30)
-
-    except Exception as e:
-        print("Error:", e)
-        time.sleep(10)
+    print("Scanning market...")
+    time.sleep(30)
